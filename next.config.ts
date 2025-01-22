597b44037e207ffeee5213fa09efb695546d5290
loac <admin@loac.cc>
2025-01-22 16:19:13 +0800

    feat: 初始化 Next 项目.

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  /* 开启后导出静态 HTML 文件 */
+  // output: "export"
+};
+
+export default nextConfig;
